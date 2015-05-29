52decc0bc6c979ad8a6246ad9ea79c652c25fd0e
Kevin Zhang <kevinzhang@Kevins-MacBook-Air.local>
2015-05-29 10:53:34 -0700

    first commit

---
+++ fitplan-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fitplan-swift
+//
+//  Created by Kevin Zhang on 2015-05-29.
+//  Copyright (c) 2015 Kevin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
